7f1a3cd36afdd3479046fe5e9b7596af5ace88ae
Ritvikg08 <39644231+Ritvikg08@users.noreply.github.com>
2019-03-29 18:17:12 +0530

    Add files via upload

---
+++ CodeChef.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ class CodeChef {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int i=x;
+		while(i!=42) {
+			System.out.println(i);
+		i=sc.nextInt();	
+		}
+		
+	}
+
+}
